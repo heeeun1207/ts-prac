6753224f185deade379e2939283b562c2257c3f4
jungheeeun <jungheeeun272@gmail.com>
2023-05-16 11:39:34 +0900

    할당에 대한 에러를 바로 알 수있다 .

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
-const a =1;
+const a ="1";
 const b= 2;
-function c(a , b) {
-  //a 가 any 타입인거맞아? 라고 물어보는것이다. 
+function c(a : number , b :number) {
+  //불안정한 코드라고 알려주는것 : a 가 any 타입인지 확인해볼래? 라고 물어보는것이다. 
   return a + b
 }
 console.log(c(a,b));
