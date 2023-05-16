6251cca49fe1a9aee44068db9d95550213e06c78
jungheeeun <jungheeeun272@gmail.com>
2023-05-16 11:40:44 +0900

    return 값이 number임을 명시하는것

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
-const a ="1";
+const a =1;
 const b= 2;
-function c(a : number , b :number) {
+function c(a : number , b :number) :number {
   //불안정한 코드라고 알려주는것 : a 가 any 타입인지 확인해볼래? 라고 물어보는것이다. 
   return a + b
 }
