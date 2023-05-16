c2c91f7892e91a98644a0af315d96ac2fdfd17a2
jungheeeun <jungheeeun272@gmail.com>
2023-05-16 11:52:33 +0900

    :void 회피하다 -> 리턴이 없는 함수라는걸 알 수 있다

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var a = 1;
 var b = 2;
 function c(a, b) {
+    //불안정한 코드라고 알려주는것 : a 가 any 타입인지 확인해볼래? 라고 물어보는것이다. 
     return a + b;
 }
 console.log(c(a, b));
--- test.ts
+++ test.ts
@@ -1,5 +1,12 @@
 const a =1;
 const b= 2;
+
+
+function moreFunction() :void {
+  console.log("hi");
+}
+//void : 회피하다 -> 리턴이 없는 함수이다 알 수 있음 
+
 function c(a : number , b :number) :number {
   //불안정한 코드라고 알려주는것 : a 가 any 타입인지 확인해볼래? 라고 물어보는것이다. 
   return a + b
